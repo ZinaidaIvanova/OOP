95bc6b3f6e914303f7ce0a4bd78d02305920ae0b
ZinaidaIvanova <zinaivg@inbox.ru>
2018-02-17 20:16:37 +0300

    Добавлена проверка открытия файла

--- LB1/LB1/Test.bat
+++ LB1/LB1/Test.bat
@@ -1,14 +1,20 @@
 set PROGRAM="%~1"
 
+echo test 1
 %PROGRAM% > nul
 if NOT ERRORLEVEL 1 goto err
 
-%PROGRAM% 
+echo test 2
+%PROGRAM% test-data\text.txt "search string"
+if ERRORLEVEL 1 goto err 
+
+echo test 3
+%PROGRAM% not-exist-file.txt "search string" > nul
+if NOT ERRORLEVEL 1 goto err
+
+echo success testing
+exit 0
 
 :err
 echo program testing failed
 exit 1
-
-:success
-echo success testing
-exit 0
