4ac356120ded709101270059ce9e3f450441b502
ZinaidaIvanova <zinaivg@inbox.ru>
2018-02-18 17:04:43 +0300

    Добавлена проверка на выполнение поиска несуществующей строки

--- LB1/LB1/Test.bat
+++ LB1/LB1/Test.bat
@@ -12,6 +12,10 @@ echo test 3
 %PROGRAM% not-exist-file.txt "search string" > nul
 if NOT ERRORLEVEL 1 goto err
 
+echo test 4
+%PROGRAM% test-data\example.txt "cat" > nul
+if NOT ERRORLEVEL 1 goto err
+
 echo success testing
 exit 0
 
