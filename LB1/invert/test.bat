6f25acf26af3b5c0d1ae73dc05110334fd00b0ae
ZinaidaIvanova <zinaivg@inbox.ru>
2018-03-01 19:03:24 +0300

    для invert добавлена проверка данных входного файла

--- LB1/invert/test.bat
+++ LB1/invert/test.bat
@@ -8,6 +8,10 @@ echo test 2
 %PROGRAM% not-exist-file.txt > nul
 if NOT ERRORLEVEL 1 goto err
 
+echo test 3
+%PROGRAM% test-data\empty_file.txt > nul
+if NOT ERRORLEVEL 1 goto err
+
 echo success testing
 exit 0
 
