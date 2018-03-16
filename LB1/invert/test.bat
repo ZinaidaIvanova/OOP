a288e0ec39341c0b30f33873916fb824df903f39
ZinaidaIvanova <zinaivg@inbox.ru>
2018-03-16 23:08:32 +0300

    Добавлено вычисление обратной матрицы

--- LB1/invert/test.bat
+++ LB1/invert/test.bat
@@ -12,6 +12,18 @@ echo test 3
 %PROGRAM% test-data\empty_file.txt > nul
 if NOT ERRORLEVEL 1 goto err
 
+echo test 4
+%PROGRAM% test-data\nondegenerate_matrix.txt > "%TEMP%\result.txt"
+if ERRORLEVEL 1 goto err
+fc.exe "%TEMP%\result.txt" test-data\nondegenerate_matrix_result.txt > nul
+if ERRORLEVEL 1 goto err
+
+echo test 5
+%PROGRAM% test-data\degenerate_matrix.txt > "%TEMP%\result.txt"
+if NOT ERRORLEVEL 1 goto err
+fc.exe "%TEMP%\result.txt" test-data\degenerate_matrix_result.txt > nul
+if ERRORLEVEL 1 goto err
+
 echo success testing
 exit 0
 
