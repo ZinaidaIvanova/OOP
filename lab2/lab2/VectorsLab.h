917f0a86e338304e50ab24c212bae7ef335ff641
ZinaidaIvanova <zinaivg@inbox.ru>
2018-04-13 22:18:07 +0300

    Лб 2, з.1. Добавлен тест для проверки функции нахождения минимального элемента

---
+++ lab2/lab2/VectorsLab.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <vector>
+
+std::vector<double> numbers();
+
+double GetMinElem(const std::vector<double>& numArr);
---
+++ lab2/lab2/vectorsLab.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include "VectorsLab.h"
+
+
+std::vector<double> numbers()
+{
+	std::vector<double> inputNum(std::istream_iterator<double>(std::cin), (std::istream_iterator<double>()));
+	return inputNum;
+}
+
+double GetMinElem(const std::vector<double>& numArr)
+{
+	return 1;
+}
+;
