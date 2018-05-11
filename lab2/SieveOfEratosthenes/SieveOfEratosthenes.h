1e5f24a73946efb7a92f1967f6034791e69918d3
ZinaidaIvanova <zinaivg@inbox.ru>
2018-05-11 22:46:51 +0300

    #5
    Функция ArePrimeNumber(const int &upperBound) переименована в PrimeNumbersSelection(const int upperBound), переменная

--- lab2/SieveOfEratosthenes/SieveOfEratosthenes.h
+++ lab2/SieveOfEratosthenes/SieveOfEratosthenes.h
@@ -2,10 +2,10 @@
 
 #include <set>
 
-bool AreInvalidArgument(int argc, char * argv[], int &upperBound);
+bool AreInvalidArgument(int argc, char * argv[], int upperBound);
 
-std::vector<bool> ArePrimeNumber(const int &upperBound);
+std::vector<bool> PrimeNumbersSelection(const int upperBound);
 
-std::set<int> GeneratePrimeNumbersSet(const int &upperBound);
+std::set<int> GeneratePrimeNumbersSet(const int upperBound);
 
 void PrintPrimeNumbersSet(const std::set<int> &PrimeNumbersSet);
