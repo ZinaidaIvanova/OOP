4ffe2536fdf745c7ff952e86018d7e1de4a374d1
ZinaidaIvanova <zinaivg@inbox.ru>
2018-06-01 18:05:15 +0300

    Реализованы методы класса CCircle

--- lab4/Shape/CCircle.cpp
+++ lab4/Shape/CCircle.cpp
@@ -4,31 +4,44 @@
 #include <cmath>
 
 
-CCircle::CCircle(CPoint center, double radius)
+CCircle::CCircle(CPoint center, double radius):
+m_center(center),
+m_radius(radius)
 {
 }
 
 double CCircle::GetArea() const
 {
-	return 0.0;
+	return m_radius * m_radius*M_PI;
 }
 
 double CCircle::GetPerimeter() const
 {
-	return 0.0;
+	return 2*m_radius*M_PI;
 }
 
 std::string CCircle::ToString() const
 {
+	/*std::ostringstream str;
+	str << std::fixed << std::setprecision(1);
+	str << "Circle" << std::endl
+		<< "Center: (" << GetCenter().x() << ", " << GetCenter().y() << ")" << std::endl
+		<< "Radius: " << GetRadius() << std::endl
+		<< "Area: " << GetArea() << std::endl
+		<< "Perimeter: " << GetPerimeter << std::endl
+		<< "Line color: " << GetOutlineColor() << std::endl
+		<< "Fill color: " << GetFillColor() << std::endl;
+	return str.str();*/
+	
 	return std::string();
 }
 
 CPoint CCircle::GetCenter() const
 {
-	return CPoint(0, 0);
+	return m_center;
 }
 
 double CCircle::GetRadius() const
 {
-	return 0.0;
+	return m_radius;
 }
